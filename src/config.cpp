7b22fe2095c95b31b18a8ad4e25a5acd1c67b521
fitenne <bluehov@gmail.com>
2022-02-19 06:41:14 -0500

    更多帮助信息

--- src/config.cpp
+++ src/config.cpp
@@ -30,6 +30,9 @@ static const int OPTION_KEY_ROBIND = 'R';
 static const int OPTION_KEY_SYMLNK = 's';
 static const int OPTION_KEY_TMPFS = 3800;
 
+static const int OPTION_GRP_HELP = 3;
+static const int OPTION_KEY_DEFT = 4000;
+
 static argp_option options[]{
     // {"chroot", OPTION_KEY_CHROOT, "newroot", 0,
     //  "chroot. an existing folder or you need has permission to create it. "
@@ -76,15 +79,13 @@ static argp_option options[]{
      "additional mount tmpfs at dest with option. can be specified multiple "
      "times",
      OPTION_GRP_CONTAINER},
+    {"default", OPTION_KEY_DEFT, 0, 0, "check default value", OPTION_GRP_HELP},
     {0, 0, 0, 0, 0, 0},
 };
 
 static const char long_help[] =
-    "example: yamc -- echo 233\vdefault argument: -c /tmp/yamc<pid> --chdir / "
-    "--real 10 -t 3 -m "
-    "134217728 --fsize 10485760 --pid 16 --nfd 16 --host yamc --domain yamc -u "
-    "<current_uid> -g <current_gid> -e PATH=/usr/bin:/bin -ro <default_robind> "
-    "-rw <default_rwbind>";
+    "example: yamc -- echo 233\vuse `yamc --default` to check some default "
+    "value";
 
 static std::string key2str(int key) {
     if (key > 4000) return "";
@@ -137,11 +138,14 @@ static std::string key2str(int key) {
         case OPTION_KEY_ROBIND:
             return "ROBIND";
             break;
+        case OPTION_KEY_SYMLNK:
+            return "SYMLNK";
+            break;
         case OPTION_KEY_TMPFS:
             return "TMPFS";
             break;
-        case OPTION_KEY_SYMLNK:
-            return "SYMLNK";
+        case OPTION_KEY_DEFT:
+            return "DEFAULT";
             break;
         default:
             break;
@@ -149,120 +153,153 @@ static std::string key2str(int key) {
     return "";
 }
 
-static auto parser = [](int key, char* arg, argp_state* state) -> error_t {
-    auto conf = (Config*)state->input;
+static void printDefaultValue() {
+    printf("default robind mount:\n");
+    for (const auto &bind : Config::default_robind) {
+        printf("\t%s -> %s\n", bind.src.c_str(), bind.dest.c_str());
+    }
+    printf("default rwbind mount:\n");
+    for (const auto &bind : Config::default_rwbind) {
+        printf("\t%s -> %s\n", bind.src.c_str(), bind.dest.c_str());
+    }
+    printf("default tmpfs mount:\n");
+    for (const auto &tmp : Config::default_tmpfs) {
+        printf("\t%s with option: %s\n", tmp.dest.c_str(), tmp.option.c_str());
+    }
+    printf("default symlink:\n");
+    for (const auto &link : Config::default_symlink) {
+        printf("\t%s -> %s\n", link.src.c_str(), link.dest.c_str());
+    }
+    printf("default environment:\n");
+    for (const auto &env : Config::default_env) {
+        printf("\t%s\n", env.c_str());
+    }
+}
+
+static auto parser = [](int key, char *arg, argp_state *state) -> error_t {
+    auto conf = (Config *)state->input;
 
     RAW_DLOG(INFO, "parsed arg: key=%s, val=%s", key2str(key).c_str(), arg);
     unsigned long ulval;
     static const int buf_sz = 1024;
     char src[buf_sz], dest[buf_sz], option[buf_sz];
-    try {
-        switch (key) {
-            // case OPTION_KEY_CHROOT:
-            //     if (strcmp(arg, "/") == 0) {
-            //         // disallow chroot to "/"
-            //         return EINVAL;
-            //     }
-            //     conf->chroot_path = arg;
-            //     break;
-            case OPTION_KEY_CHDIR:
-                conf->chdir_path = arg;
-                break;
-            case OPTION_KEY_STDOUT:
-                ulval = strtoul(arg, nullptr, 10);
-                if (errno != 0) return errno;
-                if (ulval <= 0) return EINVAL;
-                conf->stdout_fd = ulval;
-                break;
-            case OPTION_KEY_LIMIT_REAL_TIME:
-                ulval = strtoul(arg, nullptr, 10);
-                if (errno != 0) return errno;
-                if (ulval <= 0) return EINVAL;
-                conf->real_time_limit = std::chrono::seconds(ulval);
-                break;
-            case OPTION_KEY_LIMIT_CPU_TIME:
-                ulval = strtoul(arg, nullptr, 10);
-                if (errno != 0) return errno;
-                if (ulval <= 0) return EINVAL;
-                conf->cpu_time_limit = std::chrono::seconds(ulval);
-                break;
-            case OPTION_KEY_LIMIT_MEMORY:
-                ulval = strtoul(arg, nullptr, 10);
-                if (errno != 0) return errno;
-                if (ulval <= 0) return EINVAL;
-                conf->memory_limit = ulval;
-                break;
-            case OPTION_KEY_LIMIT_PID:
-                ulval = strtoul(arg, nullptr, 10);
-                if (errno != 0) return errno;
-                if (ulval <= 0) return EINVAL;
-                conf->memory_limit = ulval;
-                break;
-            case OPTION_KEY_LIMIT_OPENFD:
-                ulval = strtoul(arg, nullptr, 10);
-                if (errno != 0) return errno;
-                if (ulval < 3) return EINVAL;
-                conf->openfile_limit = ulval;
-                break;
-            case OPTION_KEY_ENV:
-                conf->env.emplace_back(arg);
-                break;
-            case OPTION_KEY_HOSTNAME:
-                conf->uts.hostname = arg;
-                break;
-            case OPTION_KEY_DOMAINNAME:
-                conf->uts.domainname = arg;
-                break;
-            case OPTION_KEY_UID:
-                ulval = strtoul(arg, nullptr, 10);
-                if (errno != 0) return errno;
-                if (ulval == 0) return EINVAL;
-                conf->use_uid.inside_id = 1;
-                conf->use_uid.outside_id = ulval;
-                conf->use_uid.count = 1;
-                break;
-            case OPTION_KEY_GID:
-                ulval = strtoul(arg, nullptr, 10);
-                if (errno != 0) return errno;
-                if (ulval == 0) return EINVAL;
-                conf->use_gid.inside_id = 1;
-                conf->use_gid.outside_id = ulval;
-                conf->use_gid.count = 1;
-                break;
-            case OPTION_KEY_ROBIND:
-                if (strnlen(arg, buf_sz) + 1 > buf_sz) return E2BIG;
-                if (sscanf(arg, "%[^:]:%s", src, dest) != 2) {
-                    return EINVAL;
-                }
-                conf->robind.emplace_back(src, dest, "",
-                                          MountPt::MNT_TYPE::ROBIND);
-                break;
-            case OPTION_KEY_RWBIND:
-                if (strnlen(arg, buf_sz) + 1 > buf_sz) return E2BIG;
-                if (sscanf(arg, "%[^:]:%s", src, dest) != 2) {
-                    return EINVAL;
-                }
-                conf->robind.emplace_back(src, dest, "",
-                                          MountPt::MNT_TYPE::RWBIND);
-                break;
-            case OPTION_KEY_SYMLNK:
-                if (strnlen(arg, buf_sz) + 1 > buf_sz) return E2BIG;
-                if (sscanf(arg, "%[^:]:%s", src, dest) != 2) {
-                    return EINVAL;
-                }
-                conf->symlink.emplace_back(src, dest);
-                break;
-            case OPTION_KEY_TMPFS:
-                if (strnlen(arg, buf_sz) + 1 > buf_sz) return E2BIG;
-                if (sscanf(arg, "%[^:]:%s", dest, option) != 2) {
-                    return EINVAL;
-                }
-                conf->rwbind.emplace_back("", dest, option,
-                                          MountPt::MNT_TYPE::TMPFS);
-                break;
-        }
-    } catch (const std::exception& e) {
-        return EINVAL;
+    switch (key) {
+        // case OPTION_KEY_CHROOT:
+        //     if (strcmp(arg, "/") == 0) {
+        //         // disallow chroot to "/"
+        //         return EINVAL;
+        //     }
+        //     conf->chroot_path = arg;
+        //     break;
+        case OPTION_KEY_CHDIR:
+            conf->chdir_path = arg;
+            break;
+        case OPTION_KEY_STDOUT:
+            ulval = strtoul(arg, nullptr, 10);
+            if (errno != 0)
+                argp_failure(state, EXIT_FAILURE, errno, "overflow");
+            if (ulval <= 0) return EINVAL;
+            conf->stdout_fd = ulval;
+            break;
+        case OPTION_KEY_LIMIT_REAL_TIME:
+            ulval = strtoul(arg, nullptr, 10);
+            if (errno != 0)
+                argp_failure(state, EXIT_FAILURE, errno, "overflow");
+            if (ulval <= 0) return EINVAL;
+            conf->real_time_limit = std::chrono::seconds(ulval);
+            break;
+        case OPTION_KEY_LIMIT_CPU_TIME:
+            ulval = strtoul(arg, nullptr, 10);
+            if (errno != 0)
+                argp_failure(state, EXIT_FAILURE, errno, "overflow");
+            if (ulval <= 0) return EINVAL;
+            conf->cpu_time_limit = std::chrono::seconds(ulval);
+            break;
+        case OPTION_KEY_LIMIT_MEMORY:
+            ulval = strtoul(arg, nullptr, 10);
+            if (errno != 0)
+                argp_failure(state, EXIT_FAILURE, errno, "overflow");
+            if (ulval <= 0) return EINVAL;
+            conf->memory_limit = ulval;
+            break;
+        case OPTION_KEY_LIMIT_PID:
+            ulval = strtoul(arg, nullptr, 10);
+            if (errno != 0)
+                argp_failure(state, EXIT_FAILURE, errno, "overflow");
+            if (ulval <= 0) return EINVAL;
+            conf->memory_limit = ulval;
+            break;
+        case OPTION_KEY_LIMIT_OPENFD:
+            ulval = strtoul(arg, nullptr, 10);
+            if (errno != 0)
+                argp_failure(state, EXIT_FAILURE, errno, "overflow");
+            if (ulval < 3) return EINVAL;
+            conf->openfile_limit = ulval;
+            break;
+        case OPTION_KEY_ENV:
+            conf->env.emplace_back(arg);
+            break;
+        case OPTION_KEY_HOSTNAME:
+            conf->uts.hostname = arg;
+            break;
+        case OPTION_KEY_DOMAINNAME:
+            conf->uts.domainname = arg;
+            break;
+        case OPTION_KEY_UID:
+            ulval = strtoul(arg, nullptr, 10);
+            if (errno != 0)
+                argp_failure(state, EXIT_FAILURE, errno, "overflow");
+            if (ulval == 0) return EINVAL;
+            conf->use_uid.inside_id = 1;
+            conf->use_uid.outside_id = ulval;
+            conf->use_uid.count = 1;
+            break;
+        case OPTION_KEY_GID:
+            ulval = strtoul(arg, nullptr, 10);
+            if (errno != 0)
+                argp_failure(state, EXIT_FAILURE, errno, "overflow");
+            if (ulval == 0) return EINVAL;
+            conf->use_gid.inside_id = 1;
+            conf->use_gid.outside_id = ulval;
+            conf->use_gid.count = 1;
+            break;
+        case OPTION_KEY_ROBIND:
+            if (strnlen(arg, buf_sz) + 1 > buf_sz)
+                argp_failure(state, EXIT_FAILURE, errno, "option to long");
+            if (sscanf(arg, "%[^:]:%s", src, dest) != 2) {
+                return EINVAL;
+            }
+            conf->robind.emplace_back(src, dest, "", MountPt::MNT_TYPE::ROBIND);
+            break;
+        case OPTION_KEY_RWBIND:
+            if (strnlen(arg, buf_sz) + 1 > buf_sz)
+                argp_failure(state, EXIT_FAILURE, errno, "option to long");
+            if (sscanf(arg, "%[^:]:%s", src, dest) != 2) {
+                return EINVAL;
+            }
+            conf->robind.emplace_back(src, dest, "", MountPt::MNT_TYPE::RWBIND);
+            break;
+        case OPTION_KEY_SYMLNK:
+            if (strnlen(arg, buf_sz) + 1 > buf_sz)
+                argp_failure(state, EXIT_FAILURE, errno, "option to long");
+            if (sscanf(arg, "%[^:]:%s", src, dest) != 2) {
+                return EINVAL;
+            }
+            conf->symlink.emplace_back(src, dest);
+            break;
+        case OPTION_KEY_TMPFS:
+            if (strnlen(arg, buf_sz) + 1 > buf_sz)
+                argp_failure(state, EXIT_FAILURE, errno, "option to long");
+            if (sscanf(arg, "%[^:]:%s", dest, option) != 2) {
+                return EINVAL;
+            }
+            conf->rwbind.emplace_back("", dest, option,
+                                      MountPt::MNT_TYPE::TMPFS);
+            break;
+        case OPTION_KEY_DEFT:
+            printDefaultValue();
+            argp_usage(state);
+            break;
     }
     return 0;
 };
@@ -272,7 +309,7 @@ static const ::argp argp = {
     nullptr, nullptr,
 };
 
-static void fillDefaultValue(Config& conf) {
+static void fillDefaultValue(Config &conf) {
     if (conf.chroot_path.empty()) {
         conf.chroot_path = "/tmp/yamc" + std::to_string(getpid());
     }
@@ -294,7 +331,7 @@ static void fillDefaultValue(Config& conf) {
     }
 }
 
-Config parseOptions(int argc, char* argv[]) {
+Config parseOptions(int argc, char *argv[]) {
     // conf with partional default value
     auto conf = Config{};
 
